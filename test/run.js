d791c3f42e4cad491487f7d48247ff834dd19373
ben <ben@dreamerslab.com>
2011-12-20 21:17:44 +0800

    init commit

---
+++ test/run.js
@@ -0,0 +1,24 @@
+var thunder, input, output;
+
+thunder = require( '../lib/thunder' );
+input   = require( 'fs' ).readFileSync( './input.html' ).toString();
+
+ouput = thunder.compile( input )({
+  title : '[thunder] Rocks!',
+  header1 : "\'This is header 1\'",
+  header2 : 'This is header 2',
+  header3 : 'This is header 3',
+  header4 : 'This is header 4',
+  header5 : 'This is header 5',
+  header6 : 'This is header 6',
+  list : [
+    '<This is list 1>',
+    '&This is list 2',
+    '&This is list 3',
+    '&This is list 4',
+    '&This is list 5',
+    '&This is list 6'
+  ]
+});
+
+console.log( ouput );
